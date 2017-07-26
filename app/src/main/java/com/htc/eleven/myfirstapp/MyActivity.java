8a3094dec326f23e10c53bf65fb84652bd1d3577
eleven_xiang <eleven_xiang@htc.com>
2017-07-26 10:14:53 +0800

    [Android Studio][First App] and button & layout sample code.
    Symptom: N/A
    Root Cause: N/A
    Solution: N/A
    Project: N/A
    Note: N/A
    Test done by RD: N/A
    Futher testing need Q team's support: Yes

---
+++ app/src/main/java/com/htc/eleven/myfirstapp/MyActivity.java
@@ -0,0 +1,72 @@
+package com.htc.eleven.myfirstapp;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MyActivity extends AppCompatActivity {
+    private final static String TAG = "ElevenButtonLayout";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                System.out.println("I am clicked !");
+//                Log.d("eleven", "I am on clicked !");
+                Log.d(TAG, "I am on clicked now !");
+
+                Intent intent = new Intent(MyActivity.this, DisplayActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
